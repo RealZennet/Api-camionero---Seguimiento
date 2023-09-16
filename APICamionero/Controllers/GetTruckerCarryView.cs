eb60907a26def9c62bab9b10b2596979fd1cc453
damian <suffodamian02@gmail.com>
2023-09-16 16:00:28 -0300

    controlador de llevan

---
+++ APICamionero/Controllers/GetTruckerCarryView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace APICamionero.Controllers
+{
+    public class GetTruckerCarryView
+    {
+
+        public int IDTruck { get; set; }
+        public int IDBatch { get; set; }
+        public DateTime ShippmentDate { get; set; }
+    }
+}
