db5e707429aef0e20e08283debcdb8f3eed077ab
damian <suffodamian02@gmail.com>
2023-11-07 15:52:31 -0300

    feature para que cliente consulte a que lote pertenece su producto por id

---
+++ APICamionero/Controllers/GetTrackBatchWithProductIDView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace APICamionero.Controllers
+{
+    public class GetTrackBatchWithProductIDView
+    {
+        public int ProductId { get;  set; }
+        public int BatchId { get; set; }
+        public string StreetDestination { get;  set; }
+        public string DoorNumber { get; set; }
+    }
+}
---
+++ APICamionero/Models/TrackBatchWithProductIDModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace APICamionero.Models
+{
+    public class TrackBatchWithProductIDModel : DatabaseConnector
+    {
+        public int ProductId { get; private set; }
+        public int BatchId { get; private set; }
+        public string StreetDestination { get; private set; }
+        public string DoorNumber { get; private set; }
+        public string CornerDestination { get; private set; }
+        public string BatchStatus { get; private set; }
+
+        public void GetBatchByProductId(int productId)
+        {
+            try
+            {
+                this.Command.CommandText = $"SELECT p.id_prod, l.id_lote, d.calle, d.num, d.esq, t.estatus " +
+                                           "FROM producto AS p " +
+                                           "JOIN integra AS i ON p.id_prod = i.id_prod " +
+                                           "JOIN lote AS l ON i.id_lote = l.id_lote " +
+                                           "JOIN transporta AS t ON l.id_lote = t.id_lote " +
+                                           "JOIN destino AS d ON t.id_des = d.id_des " +
+                                           $"WHERE p.id_prod = {productId}";
+                this.Reader = this.Command.ExecuteReader();
+
+                if (this.Reader.Read())
+                {
+                    ProductId = Convert.ToInt32(this.Reader["id_prod"]);
+                    BatchId = Convert.ToInt32(this.Reader["id_lote"]);
+                    StreetDestination = this.Reader["calle"].ToString();
+                    DoorNumber = this.Reader["num"].ToString();
+                    CornerDestination = this.Reader["esq"].ToString();
+                }
+
+                this.Reader.Close();
+            }
+            catch (SqlException ex)
+            {
+                throw new Exception("Error: " + ex.Message);
+            }
+        }
+
+    }
+}
